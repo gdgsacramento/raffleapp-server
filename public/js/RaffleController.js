ea0e4df494311a20afc7af802441f7a53ad11ff3
Thomas Amsler <tamsler@gmail.com>
2013-01-06 00:09:01 -0800

    Move all styles to main.css. Added raffle selection background color change. Showing tickets for each raffle.

--- public/js/RaffleController.js
+++ public/js/RaffleController.js
@@ -1,49 +1,77 @@
 
-var myApp = angular.module('myApp',[]);
+/*
+ * http://docs.angularjs.org/guide/module
+ */
+var myAppModule = angular.module('myApp',[]);
+
 
 function RaffleController($scope, $http) {
 
-  $scope.raffle = { raffle_name : ""};
+    var selectedRaffleIndex = 0;
+    $scope.ticket = { raffle_id: "", user_name : ""};
 
-  $scope.ticket = { raffle_id: "", user_name : ""};
+    $http.get('/api/v1/raffle').success(function(data) {
 
-  //{"raffle_id":"505f78b7e6c235e253000001", "user_name":"thomas"}
+        /*
+         * Init isSelected for CSS selection background color
+         */
+        for(var i = 0; i < data.length; i++) {
 
-	$http.get('/api/v1/raffle').success(function(data) {
+            data[i].isSelected = "not";
+        }
 
-    	$scope.raffles = data;
-  	});
+        $scope.raffles = data;
+    });
 
-  	$scope.selectRaffle = function(raffleId) {
+    $scope.selectRaffle = function(raffleId, raffleIndex) {
 
-      $scope.ticket.raffle_id = raffleId;
-      console.dir($scope.ticket);
+        $scope.ticket.raffle_id = raffleId;
+        selectedRaffleIndex = raffleIndex;
 
-  	};
+        /*
+         * Reset selected raffle background color
+         */
+        for(var i = 0; i < $scope.raffles.length; i++) {
 
-  	$scope.createRaffle = function() {
+            $scope.raffles[i].isSelected = "not";
+        }
 
-  		$http.post('/api/v1/raffle', $scope.raffle).success(function(data) {
-  			$scope.raffles.push(data[0]);
+        /*
+         * Change selected raffle's background color
+         */
+        $scope.raffles[selectedRaffleIndex].isSelected = "is";
+    };
+
+    $scope.createRaffle = function() {
+
+        $http.post('/api/v1/raffle', $scope.raffle).success(function(data) {
+
+            $scope.raffles.push(data[0]);
             $scope.raffle = { raffle_name : ""};
-  		});
-  	}
+        });
+    }
 
     $scope.deleteRaffle = function(raffle) {
+
         $http.delete('/api/v1/raffle/'+raffle._id).success(function(data) {
+
             $scope.raffles.splice($scope.raffles.indexOf(raffle), 1); //indexOf not supported on IE
         });
     }
 
     $scope.createTicket = function() {
 
-      console.log($scope.ticket.user_name);
+        console.log($scope.ticket.user_name);
 
-      $http.post('/api/v1/ticket', $scope.ticket).success(function(data) {
-          console.log("POST Ticket");
-          console.dir(data);
-      });
+        $http.post('/api/v1/ticket', $scope.ticket).success(function(data) {
+
+            $scope.raffles[selectedRaffleIndex].tickets.push($scope.ticket.user_name);
+            $scope.ticket.user_name = "";
+        });
     }
 }
 
+/*
+ * Dependency injection: http://docs.angularjs.org/guide/di
+ */
 RaffleController.$inject = ['$scope', '$http'];
