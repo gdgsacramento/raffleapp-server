a9e8c86e902d2fe916db4090e7515d0a655c543c
Juan Sanchez <juansg.eng@gmail.com>
2016-11-12 13:26:10 -0800

    Changed angular module imports

--- app/js/app.js
+++ app/js/app.js
@@ -5,10 +5,10 @@
  */
 
 // Declare app level module which depends on filters, services, and directives
-var ra = angular.module('ra', ['firebase','ra.filters', 'ra.services', 'ra.directives', 'ra.mock.services', 'ngRoute']).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+	var ra = angular.module('ra', ['firebase','ra.filters', 'ra.services', 'ra.directives', 'ra.mock.services', 'ngRoute'])
+		.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider.when('/landing', {templateUrl: 'partials/landing.html', controller: LandingController});
     $routeProvider.when('/', {templateUrl: 'partials/raffle.html', controller: RaffleController});
-    $routeProvider.when('/admin', {templateUrl: 'partials/admin.html', controller: AdminController});
     $routeProvider.otherwise({redirectTo: '/'});
 
     $locationProvider.html5Mode(true);
