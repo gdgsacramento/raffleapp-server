74836b3248695ce07a3dfaffebe88c8d8ba0ed4b
Oleg Kalugin <olegspace@gmail.com>
2018-10-11 15:38:30 -0700

    fix tests and push the regular monthly bump to make functions work, sigh

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -2,14 +2,32 @@ const test = require('firebase-functions-test')();
 const sinon = require('sinon');
 const assert = require('assert');
 const admin = require('firebase-admin');
+const nock = require('nock');
 
 
 describe('Raffle functions', function () {
+    before(function () {
+        nock.disableNetConnect();
+    });
+
     afterEach(function () {
         sinon.restore();
     });
 
+    after(function () {
+        nock.enableNetConnect();
+    });
+
     it('should draw winners', async function () {
+        nock('https://www.googleapis.com')
+            .post('/oauth2/v4/token')
+            .reply(200, {
+                access_token: 'access_token',
+                id_token: 'id_token',
+                expires_in: 1,
+                token_type: 'Bearer'
+            });
+
         const raffleFunctions = require('../index.js');
 
         const wrapped = test.wrap(raffleFunctions.drawWinners);
@@ -23,9 +41,8 @@ describe('Raffle functions', function () {
                 obj2: {name: 'doe'}
             })
         });
-        const winnersStub = sinon.stub(admin.database.Reference.prototype, 'set').callsFake(winners => {
+        const winnersStub = sinon.stub(admin.database.Reference.prototype, 'set').callsFake(async winners => {
             assert.deepStrictEqual(winners.sort(), ['doe', 'john']); // order is not important
-            return Promise.resolve();
         });
         await wrapped(test.makeChange(before, after));
         assert(winnersStub.called, true);
